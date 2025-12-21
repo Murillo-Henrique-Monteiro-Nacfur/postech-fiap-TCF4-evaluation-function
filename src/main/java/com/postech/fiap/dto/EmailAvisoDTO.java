d663762b38239b93c73a8007189cf9b4b3790b66
Murillo-Henrique-Monteiro-Nacfur <murillonacfur@gmail.com>
2025-12-21 12:00:34 -0300

    Ajuste serialização EmailAvisoDTO

--- src/main/java/com/postech/fiap/dto/EmailAvisoDTO.java
+++ src/main/java/com/postech/fiap/dto/EmailAvisoDTO.java
@@ -7,9 +7,36 @@ public class EmailAvisoDTO {
     private String message;
     private String subject;
 
+    public EmailAvisoDTO() {
+    }
+
     public EmailAvisoDTO(List<String> destinations, String message, String subject) {
         this.destinations = destinations;
         this.message = message;
+        this.subject = subject;
+    }
+
+    public List<String> getDestinations() {
+        return destinations;
+    }
+
+    public void setDestinations(List<String> destinations) {
+        this.destinations = destinations;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
         this.subject = subject;
     }
 }
