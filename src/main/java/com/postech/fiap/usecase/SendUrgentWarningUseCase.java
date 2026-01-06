d38cbd712e42969bb8b63cee8629cc876aa9248f
Murillo-Henrique-Monteiro-Nacfur <murillonacfur@gmail.com>
2026-01-06 20:55:20 -0300

    Ajuste para enviar notificação com data e hora formatados e ajustes no application.properties.

--- src/main/java/com/postech/fiap/usecase/SendUrgentWarningUseCase.java
+++ src/main/java/com/postech/fiap/usecase/SendUrgentWarningUseCase.java
@@ -9,6 +9,8 @@ import com.postech.fiap.model.AdministratorEntity;
 import com.postech.fiap.model.EvaluationEntity;
 import jakarta.enterprise.context.ApplicationScoped;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @ApplicationScoped
@@ -16,6 +18,8 @@ public class SendUrgentWarningUseCase {
 
     private static final String SUBJECT_URGENT_EVALUATION_WITH_LOW_RATING = "[URGENTE] - Avaliação com nota baixa";
     private static final String MESSAGE_TEMPLATE = "Atenção! Uma nova avaliação com nota baixa foi registrada.\n Descrição: %s\n Nota: %d\n Data e Hora: %s";
+    private static final String DD_MM_YYYY_HH_MM_SS = "dd/MM/yyyy HH:mm:ss";
+    private static final String DATE_NOT_AVAILABLE = "Data não disponível";
 
     private final UrgentWarningGateway urgentWarningGateway;
 
@@ -27,22 +31,33 @@ public class SendUrgentWarningUseCase {
         String message = String.format(MESSAGE_TEMPLATE,
                 newEvaluation.getDescription(),
                 newEvaluation.getRating(),
-                newEvaluation.getDateHourCriation());
+                formatDate(newEvaluation.getDateHourCriation()));
 
         List<String> emails = getEmailsAdministrators();
         EmailAvisoDTO emailAvisoDTO = new EmailAvisoDTO(emails, message, SUBJECT_URGENT_EVALUATION_WITH_LOW_RATING);
-        String avisoUrgenciaJson;
-        try {
-            avisoUrgenciaJson = new ObjectMapper().writeValueAsString(emailAvisoDTO);
-        } catch (JsonProcessingException e) {
-            throw new EvaluationFunctionException("Erro ao gerar aviso de urgencia", "JSON_PROCESSING_ERROR");
-        }
+        String avisoUrgenciaJson = getAvisoUrgenciaJson(emailAvisoDTO);
         urgentWarningGateway.sendWarning(avisoUrgenciaJson);
 
     }
 
+    private String getAvisoUrgenciaJson(EmailAvisoDTO emailAvisoDTO) {
+        try {
+            return new ObjectMapper().writeValueAsString(emailAvisoDTO);
+        } catch (JsonProcessingException e) {
+            throw new EvaluationFunctionException("Erro ao gerar aviso de urgencia", "JSON_PROCESSING_ERROR");
+        }
+    }
+
     private List<String> getEmailsAdministrators() {
         return AdministratorEntity.find("SELECT a.email FROM AdministratorEntity a").project(String.class).list();
+    }
+
+    private String formatDate(LocalDateTime dateTime) {
+        if (dateTime == null) {
+            return DATE_NOT_AVAILABLE;
+        }
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DD_MM_YYYY_HH_MM_SS);
+        return dateTime.format(formatter);
     }
 
 }
